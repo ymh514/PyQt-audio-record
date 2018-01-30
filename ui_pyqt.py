2a2fe78b7725488bdab17746a8de9de9e5ab5929
ymh514 <microvoyager@hotmail.com>
2018-01-30 10:12:46 +0800

    recognize native english work

--- ui_pyqt.py
+++ ui_pyqt.py
@@ -84,7 +84,7 @@ class Ui_MainWindow(object):
 "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
 "p, li { white-space: pre-wrap; }\n"
 "</style></head><body style=\" font-family:\'Ubuntu\'; font-size:11pt; font-weight:400; font-style:normal;\">\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">output1</p></body></html>"))
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">test</p></body></html>"))
         self.startBtn.setText(_translate("MainWindow", "Start"))
         self.stopBtn.setText(_translate("MainWindow", "Stop"))
 
